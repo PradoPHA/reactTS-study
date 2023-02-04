9d527439329d0b7316e624185a67c404c75e18e6
PradoPHA <phaprado57@gmail.com>
2023-02-04 20:54:04 -0300

    fix: react-router-dom update tweaks

--- aulas/src/app/routes/index.tsx
+++ aulas/src/app/routes/index.tsx
@@ -1,15 +1,13 @@
-import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom"
+import { BrowserRouter, Route, Routes as Switch, Navigate } from "react-router-dom"
 import { Dashboard } from "../pages"
 
 export const Routes = () => {
     return (
         <BrowserRouter>
             <Switch>
+                <Route path="/pagina-inicial" element={<Dashboard/>} />
 
-                <Route exact path="/pagina-inicial" component={Dashboard} />
-  
-
-                <Route path="*" component={() => <Redirect to="/pagina-inicial"/>} />
+                <Route path="*" element={<Navigate to="/pagina-inicial"/>} />
             </Switch>
         </BrowserRouter>
     )
