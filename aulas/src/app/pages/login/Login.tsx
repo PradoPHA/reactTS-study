00e53ad1db9199bc02e9a647b63a9708081b6c76
PradoPHA <phaprado57@gmail.com>
2023-02-04 22:21:49 -0300

    feat: browsing between pages

---
+++ aulas/src/app/pages/login/Login.tsx
@@ -0,0 +1,11 @@
+import {Link} from 'react-router-dom' 
+
+export const Login = () => {
+    return (
+        <div>
+            <p>Login</p>
+            
+            <Link to="pagina-inicial">Dashboard</Link>
+        </div>
+    )
+}
