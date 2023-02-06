cac05d5f7596ce8f18aab80825b3891f166cdc0d
PradoPHA <phaprado57@gmail.com>
2023-02-06 18:48:49 -0300

    feat: add login page and form

--- aulas/src/app/pages/login/Login.tsx
+++ aulas/src/app/pages/login/Login.tsx
@@ -1,11 +1,34 @@
-import {Link} from 'react-router-dom' 
+import { useState } from "react"
 
 export const Login = () => {
+
+    const [password, setPassword] = useState('');
+    const [email, setEmail] = useState('');
+
+    const handleEntrar = () => {
+        console.log('Seu e-mail é: ' + email);
+        console.log('Sua senha é: ' + password);
+    }
+
     return (
         <div>
-            <p>Login</p>
-            
-            <Link to="pagina-inicial">Dashboard</Link>
+            <form>
+
+                <label>
+                    <span>E-mail</span>
+                    <input value={email} onChange={e => setEmail(e.target.value)}/>
+                </label>
+
+                <label>
+                    <span>Senha</span>
+                    <input type="password" value={password} onChange={p => setPassword(p.target.value)}/>
+                </label>
+
+                <button type="button" onClick={handleEntrar}>
+                    Entrar
+                </button>
+                
+            </form>
         </div>
     )
 }
